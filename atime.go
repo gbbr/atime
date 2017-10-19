b2294877db246ec9b38fc6a3bb0ccc2300c0684c
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2017-10-19 19:33:53 +0200

    Added a Get method

--- atime.go
+++ atime.go
@@ -1,8 +1,10 @@
+// Package atime provides some minimal utilities in working with file access times.
 package atime
 
 import (
 	"os"
 	"sort"
+	"time"
 )
 
 type Ascending []os.FileInfo
@@ -12,3 +14,6 @@ var _ sort.Interface = (*Ascending)(nil)
 func (fis Ascending) Len() int           { return len(fis) }
 func (fis Ascending) Swap(i, j int)      { fis[i], fis[j] = fis[j], fis[i] }
 func (fis Ascending) Less(i, j int) bool { return atime(fis[i]).Before(atime(fis[j])) }
+
+// Get returns the access time from the given os.FileInfo.
+func Get(fi os.FileInfo) time.Time { return atime(fi) }
